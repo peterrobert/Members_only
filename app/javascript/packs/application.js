00bed35995f612ef640d8f7b4fb80fa00cad7a99
peterrobert <pwambui93@gmail.com>
2020-05-11 15:14:02 +0300

    added bootstrap for designing

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,16 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+// bootstrap
+import "../stylesheets/application"  
+
+import "bootstrap";
+
+document.addEventListener("turbolinks:load", () => {
+  $('[data-toggle="tooltip"]').tooltip()
+  $('[data-toggle="popover"]').popover()
+})
+
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
